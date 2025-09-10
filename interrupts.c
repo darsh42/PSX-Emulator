8cee6ff7403cc9cbbf47b653a53bfe5fec072c29
Darsh Chanduka <darcha02@e133926.arm.com>
2025-09-10 10:27:22 +0100

    INT: Add interrupts

--- interrupts.c
+++ interrupts.c
@@ -2,11 +2,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define INTERRUPTS_PRIVATE
 #include "interrupts.h"
 
+#include "cp0.h"
+#include "gpu.h"
 #include "timer.h"
 #include "memory.h"
+
+#include "trace.h"
+#define TRACE_INTERRUPTS(function, format, ...) \
+    trace(TRACE_INTERRUPTS_EN, "interrupts.c", function, format, __VA_ARGS__)
 
 static struct interrupts interrupts;
 
@@ -22,7 +27,8 @@ uint32_t read_interrupts( uint32_t address )
             break;
     }
 
-    TRACE_INTERRUPTS("read_interrupts ", "address: %08x | data: %08x\n", address, data);
+    TRACE_INTERRUPTS("read_interrupts",
+            "address: %08x | data: %08x\n", address, data);
 
     return data;
 }
@@ -38,7 +44,74 @@ void write_interrupts( uint32_t address, uint32_t data )
             break;
     }
 
-    TRACE_INTERRUPTS("write_interrupts", "address: %08x | data: %08x\n", address, data);
+    TRACE_INTERRUPTS("write_interrupts",
+            "address: %08x | data: %08x\n", address, data);
+}
+
+void interrupt_acknowledge( void ) {
+    /* get masked status value */
+    uint32_t masked_status = interrupts.interrupt_mask &
+                             interrupts.interrupt_status;
+
+    /* send i_status acknowledgement */
+    interrupts.interrupt_status ^= masked_status;
+
+    /* send device acknowledgement */
+    switch (masked_status) {
+    case IRQ1: {
+        union gpustat gpustat;
+
+        /* get current status */
+        gpu_get_gpustat(&gpustat);
+
+        /* acknowledge interrupt */
+        gpustat.interrupt_request = 0;
+
+        /* write back */
+        gpu_set_gpustat(gpustat);
+        break;
+    }
+    case IRQ2:
+    case IRQ3:
+    case IRQ7:
+    case IRQ8:
+    case IRQ9:
+    case IRQ10:
+    }
 }
 
 void init_interrupts( void ) {}
+void task_interrupts( void ) {
+    /* get masked status value */
+    uint32_t masked_status = interrupts.interrupt_mask &
+                             interrupts.interrupt_status;
+
+    if (masked_status) {
+        TRACE_INTERRUPTS("task_interrupts",
+                "handling: %04x\n", masked_status);
+
+        /* check for multiple interrupts at same time (value is
+         * not a power of 2 then there are multiple interrupts)*/
+        if ((masked_status & (masked_status - 1)) != 0) {
+            union cp0_sr    sr;
+            union cp0_cause cause;
+
+            read_cp0_reg(CP0_SR,    &sr.value);
+            read_cp0_reg(CP0_CAUSE, &cause.value);
+
+            // set current interrupt enable
+            sr.value |= (1 <<  0);
+            // set interrupt mask bit
+            sr.value |= (1 << 10);
+
+            // set pending interrupt bit
+            cause.value |= (1 << 10);
+
+            write_cp0_reg(CP0_SR,    sr.value);
+            write_cp0_reg(CP0_CAUSE, cause.value);
+        }
+
+        /* send irq request */
+        cp0_exception(INT);
+    }
+}
--- interrupts.h
+++ interrupts.h
@@ -4,28 +4,47 @@
 #include <stdint.h>
 #include <pthread.h>
 
-extern uint32_t read_interrupts( uint32_t address );
-extern void     write_interrupts( uint32_t address, uint32_t data );
+enum irq {
+    IRQ0  = 1,      // vblank
+    IRQ1  = 2,      // gpu
+    IRQ2  = 4,      // cdrom
+    IRQ3  = 8,      // dma
+    IRQ4  = 16,     // tmr0
+    IRQ5  = 32,     // tmr1
+    IRQ6  = 64,     // tmr2
+    IRQ7  = 128,    // controller_and_memory_card
+    IRQ8  = 256,    // sio
+    IRQ9  = 512,    // spu
+    IRQ10 = 1024,   // controller
+};
 
-#ifdef INTERRUPTS_PRIVATE
+union interrupt_status {
+    uint32_t value;
+    struct {
+        uint32_t vblank:                     1;
+        uint32_t gpu:                        1;
+        uint32_t cdrom:                      1;
+        uint32_t dma:                        1;
+        uint32_t tmr0:                       1;
+        uint32_t tmr1:                       1;
+        uint32_t tmr2:                       1;
+        uint32_t controller_and_memory_card: 1;
+        uint32_t sio:                        1;
+        uint32_t spu:                        1;
+        uint32_t controller:                 1;
+    };
+};
 
-#include "trace.h"
-
-#ifdef ENABLE_INTERRUPTS_TRACE
-#define TRACE_INTERRUPTS(function, format, ...) \
-    trace(TRACE_INTERRUPTS_EN, "interrupts.c", function, format, __VA_ARGS__)
-#else
-#define TRACE_INTERRUPTS(function, format, ...) 
-#endif
-
-struct interrupts
-{
+struct interrupts {
     uint32_t interrupt_status;
     uint32_t interrupt_mask;
 };
 
-#endif // INTERRUPTS_PRIVATE
-
 void init_interrupts( void );
+void task_interrupts( void );
+void interrupt_acknowledge( void );
+
+uint32_t  read_interrupts( uint32_t address );
+void     write_interrupts( uint32_t address, uint32_t data );
 
 #endif // INTERRUPTS_H_INCLUDED
